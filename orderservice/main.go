0b4aa1d7c8293df76699baa332cfa6627deb0baf
Amandeep Singh <asingh19@statestreet.com>
2020-05-21 15:41:43 +0530

    add a golang service to return order using customer id.

---
+++ orderservice/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Order struct {
+	OrderID    int
+	CustomerID int
+}
+
+var (
+	orders []Order
+)
+
+func main() {
+
+	initOrders()
+	handleRequests()
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	myRouter.HandleFunc("/orders", returnAllOrders)
+	myRouter.HandleFunc("/orders/{customerId}", returnOrder)
+
+	log.Println("Listening on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
+}
+
+func initOrders() {
+	orders = []Order{
+		Order{OrderID: 21, CustomerID: 1},
+		Order{OrderID: 31, CustomerID: 2},
+	}
+}
+
+func returnAllOrders(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllOrders")
+	json.NewEncoder(w).Encode(orders)
+}
+
+func returnOrder(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key, _ := strconv.Atoi(vars["customerId"])
+
+	for _, article := range orders {
+		if article.CustomerID == key {
+			json.NewEncoder(w).Encode(article)
+			break
+		}
+	}
+}
