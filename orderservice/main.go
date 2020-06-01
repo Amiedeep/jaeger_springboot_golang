5403a024b70b659874139cca4e8541fb495a24af
Amandeep Singh <amandeep11121993@gmail.com>
2020-06-01 13:03:03 +0530

    experiment with packages

--- orderservice/main.go
+++ orderservice/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/amiedeep/jaeger_springboot_golang/orderservice/app"
 	"github.com/gorilla/mux"
 	"github.com/opentracing-contrib/go-stdlib/nethttp"
 	"github.com/yurishkuro/opentracing-tutorial/go/lib/tracing"
@@ -20,7 +21,7 @@ func main() {
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.Path("/orders").Queries("customerID", "{customerID}").HandlerFunc(ReturnOrder)
+	myRouter.Path("/orders").Queries("customerID", "{customerID}").HandlerFunc(app.ReturnOrder)
 
 	log.Println("Listening on localhost:8081")
 	log.Fatal(http.ListenAndServe(":8081", nethttp.Middleware(tracer, myRouter)))
