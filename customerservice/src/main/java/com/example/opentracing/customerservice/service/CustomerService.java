1b357d643a7243345d54105bb480e6863bf6622b
Amandeep Singh <amandeep11121993@gmail.com>
2020-07-09 09:27:02 +0530

    remove kafka

--- customerservice/src/main/java/com/example/opentracing/customerservice/service/CustomerService.java
+++ customerservice/src/main/java/com/example/opentracing/customerservice/service/CustomerService.java
@@ -1,20 +1,19 @@
 package com.example.opentracing.customerservice.service;
 
+import static com.example.opentracing.customerservice.utils.http.getHttp;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import com.example.opentracing.customerservice.model.Customer;
 import com.example.opentracing.customerservice.repository.CustomerRepository;
-import io.opentracing.tag.Tags;
-import io.opentracing.util.GlobalTracer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.opentracing.tag.Tags;
 import io.opentracing.util.GlobalTracer;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.example.opentracing.customerservice.utils.http.getHttp;
 
 
 @Service
@@ -29,9 +28,6 @@ public class CustomerService {
     @Value("${orderservice.port}")
     private int orderServicePort;
 
-    @Autowired
-    private Producer producer;
-
     public Map<String, Object> findCustomer(long customerID) {
 
         Map<String, Object> response = new HashMap<String, Object>();
@@ -44,7 +40,6 @@ public class CustomerService {
             return response;
         }
 
-        producer.sendMessage("Found customer: " + customer.name);
         response.put("Customer", customer);
         String orders = getHttp(orderServiceHost, orderServicePort, "orders", customerID);
         response.put("Orders", orders);
@@ -65,11 +60,8 @@ public class CustomerService {
             Tags.ERROR.set(GlobalTracer.get().activeSpan(), true);
         }
 
-        producer.sendMessage("Found customer: " + customer.name);
-
         response.put("Customer", customer);
         String orders = getHttp(orderServiceHost, orderServicePort, "orders", customerID);
-        producer.sendMessage("Found customer: " + customer.name);
 
         response.put("Orders", orders);
         return response;
