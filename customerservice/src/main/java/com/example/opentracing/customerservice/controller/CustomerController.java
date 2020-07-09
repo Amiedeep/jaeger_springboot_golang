4bc1a3cdf0fcd93342cdcddb24cc3e5b67d772a2
Amandeep Singh <amandeep11121993@gmail.com>
2020-07-09 09:32:24 +0530

    remove interceptor

--- customerservice/src/main/java/com/example/opentracing/customerservice/controller/CustomerController.java
+++ customerservice/src/main/java/com/example/opentracing/customerservice/controller/CustomerController.java
@@ -1,26 +1,16 @@
 package com.example.opentracing.customerservice.controller;
 
 
-import com.example.opentracing.customerservice.model.Customer;
+import java.util.Map;
+
 import com.example.opentracing.customerservice.service.CustomerService;
-import com.example.opentracing.customerservice.service.Producer;
-import io.opentracing.Scope;
-import io.opentracing.Span;
-import io.opentracing.Tracer;
-import io.opentracing.util.GlobalTracer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.google.common.collect.ImmutableMap;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 public class CustomerController {
